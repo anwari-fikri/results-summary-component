22a277b7f4751c585065df123cb427f4abcdb898
anwari-fikri <anwari.fikri@gmail.com>
2023-03-17 10:14:02 +0800

    static design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        /* primary */
+        "light-red": "hsl(0, 100%, 67%)",
+        "orangey-yellow": "hsl(39, 100%, 56%)",
+        "green-teal": "hsl(166, 100%, 37%)",
+        "cobalt-blue": "hsl(234, 85%, 45%)",
+
         /* neutral */
         "pale-blue": "hsl(221, 100%, 96%)",
         "light-lavender": "hsl(241, 100%, 89%)",
